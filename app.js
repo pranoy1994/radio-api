2a423df92c9c32ece9a634d52ff16f4afe9a261d
Pranoy Biswas <pranoybiswas1994@yahoo.com>
2017-07-24 14:14:38 +0530

    error handling

--- app.js
+++ app.js
@@ -5,6 +5,31 @@ var app = express();
 
 app.use('/', routes);
 
+app.use(function (req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+
+if (app.get('env') === 'development') {
+    app.use(function (err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+app.use(function (err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
+
 
 app.listen(port, function(){
   console.log(`Express server listening on port ${port}`);
