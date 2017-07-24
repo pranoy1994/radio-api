e27dc3e2a584d93427b32dea3332ce019523f854
pranoy biswas <pranoybiswas1994@gmail.com>
2017-07-24 06:16:58 -0700

    404 error

--- app.js
+++ app.js
@@ -15,19 +15,14 @@ app.use(function (req, res, next) {
 if (app.get('env') === 'development') {
     app.use(function (err, req, res, next) {
         res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
+        res.send("error");
     });
 }
 
 app.use(function (err, req, res, next) {
     res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
+    res.send(err);
+    
 });
 
 
