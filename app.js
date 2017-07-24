078fead49bd2333b19e7ffcf22f00381e24da3bf
Pranoy Biswas <pranoybiswas1994@yahoo.com>
2017-07-24 13:45:35 +0530

    port for heroku deploy

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var app = express();
 
 app.use('/', routes);
 
-app.listen('3000', ()=>{
-console.log("Server is running on port 3000");
+
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
